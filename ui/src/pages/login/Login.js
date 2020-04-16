bedb5b2bcc5ba98c5ff110ecc9ffca83afdbb9c5
Leonid Rozenberg <leonid.rozenberg@digitalasset.com>
2020-04-16 13:34:38 -0400

    More UI application specification.

--- ui/src/pages/login/Login.js
+++ ui/src/pages/login/Login.js
@@ -22,7 +22,7 @@ function Login(props) {
     <Grid container className={classes.container}>
       <div className={classes.logotypeContainer}>
         <img src={logo} alt="logo" className={classes.logotypeImage} />
-        <Typography className={classes.logotypeText}>App Template</Typography>
+        <Typography className={classes.logotypeText}>DABL Chess</Typography>
       </div>
       <div className={classes.formContainer}>
         <div className={classes.form}>
