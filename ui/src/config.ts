fbf97f88b548dc086e8beb7b3dd739727544ced7
Sarah Breckenridge <40438835+Sarahb-da@users.noreply.github.com>
2021-11-29 13:51:18 -0500

    Update chess ReadMe (#17)
    * Update chess readme
    
    * replaced dabl in a few more places

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,37 +1,48 @@
-import uuidv4 from "uuid/v4";
-import * as jwt from "jsonwebtoken";
+import uuidv4 from "uuid/v4"
+import * as jwt from "jsonwebtoken"
 
-export const isLocalDev : boolean = process.env.NODE_ENV === 'development';
+export const isLocalDev: boolean = process.env.NODE_ENV === "development"
 
-export const ledgerId : string = isLocalDev ? "dablchess" : window.location.host.split('.')[0];
+export const ledgerId: string = isLocalDev ? "dablchess" : window.location.host.split(".")[0]
 
-let apiUrl : string = window.location.host + (window.location.port ? ':' + window.location.port : '');
+let apiUrl: string = window.location.host + (window.location.port ? ":" + window.location.port : "")
 
-export const httpBaseUrl : string | undefined = isLocalDev ? undefined : `https://${apiUrl}/`;
+export const httpBaseUrl: string | undefined = isLocalDev ? undefined : `https://${apiUrl}/`
 
 // Unfortunately, the development server of `create-react-app` does not proxy
 // websockets properly. Thus, we need to bypass it and talk to the JSON API
 // directly in development mode.
-export const wsBaseUrl : string | undefined = isLocalDev ? 'ws://localhost:7575/' : undefined;
+export const wsBaseUrl: string | undefined = isLocalDev ? "ws://localhost:7575/" : undefined
 
-const applicationId : string = uuidv4();
+const applicationId: string = uuidv4()
 
-export function createToken(party : string) {
-  return jwt.sign({ "https://daml.com/ledger-api": { ledgerId, applicationId, admin: true, actAs: [party], readAs: [party] } }, "secret")
+export function createToken(party: string) {
+    return jwt.sign(
+        {
+            "https://daml.com/ledger-api": {
+                ledgerId,
+                applicationId,
+                admin: true,
+                actAs: [party],
+                readAs: [party],
+            },
+        },
+        "secret"
+    )
 }
 
-export const dablLoginUrl : string = `${apiUrl}/.hub/v1/auth/login`;
+export const damlHubLoginUrl: string = `${apiUrl}/.hub/v1/auth/login`
 
-export const defaultPartiesUrl : string = `${apiUrl}/.hub/v1/default-parties`;
+export const defaultPartiesUrl: string = `${apiUrl}/.hub/v1/default-parties`
 
-export const publicTokenUrl = `https://${apiUrl}/.hub/v1/public/token`;
+export const publicTokenUrl = `https://${apiUrl}/.hub/v1/public/token`
 
 console.log(`Config:
   isLocalDev ${isLocalDev}
   ledgerId ${ledgerId}
   httpBaseUrl ${httpBaseUrl}
   wsBaseUrl ${wsBaseUrl}
-  dablLoginUrl ${dablLoginUrl}
+  damlHubLoginUrl ${damlHubLoginUrl}
   defaultPartiesUrl ${defaultPartiesUrl}
   publicTokenUrl ${publicTokenUrl}
-`);
+`)
